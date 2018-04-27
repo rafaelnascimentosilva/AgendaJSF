eb29f3bc338321d1196f2c22a0b7e22671306577
rafaelnascimentosilva <rafaelnasc.silv@gmail.com>
2018-04-26 22:53:23 -0300

    adicionando growl messages

--- src/main/java/agenda/contato/util/FacesUtil.java
+++ src/main/java/agenda/contato/util/FacesUtil.java
@@ -3,16 +3,22 @@ package agenda.contato.util;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
 
-@ManagedBean
+@ManagedBean(name = "messagesView")
 public class FacesUtil {
-	public void buttonAction(ActionEvent actionEvent) {
-		addMessage("Welcome to Primefaces!!");
-	}
-
-	public void addMessage(String summary) {
-		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null);
-		FacesContext.getCurrentInstance().addMessage(null, message);
-	}
+	  public void info() {
+	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Info", "Cadastro Realizado com Sucesso"));
+	    }
+	 
+	    public void warn() {
+	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Warning!", "Watch out for PrimeFaces."));
+	    }
+	 
+	    public void error() {
+	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", "Contact admin."));
+	    }
+	 
+	    public void fatal() {
+	        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Fatal!", "System Error"));
+	    }
 }
